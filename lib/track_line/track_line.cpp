7052aaeec8543fcb02a9aa8c3e8335fb1304bfda
HenryCROSS <henry583050973@gmail.com>
2024-02-03 17:42:23 -0500

    change speed and delay

--- lib/track_line/track_line.cpp
+++ lib/track_line/track_line.cpp
@@ -7,23 +7,23 @@ void Track_Line::run()
 {
     HW_API::read_data();
 
-    if(G::left_on_line && G::right_on_line){
+    if(!G::left_on_line && !G::right_on_line){
         HW_API::forward();
         Serial.println("on_line");
-    } else if (!G::left_on_line && G::right_on_line){
+    } else if (G::left_on_line && !G::right_on_line){
         Serial.println("left_out");
         HW_API::turn_left();
         // delay(300);
         // HW_API::emerg_stop();
         // delay(100);
-    } else if (G::left_on_line && !G::right_on_line){
+    } else if (!G::left_on_line && G::right_on_line){
         HW_API::turn_right();
         Serial.println("right_out");
         // delay(300);
         // HW_API::emerg_stop();
         // delay(100);
-    } else if (!G::left_on_line && !G::right_on_line){
-        while (!G::left_on_line || !G::right_on_line)
+    } else if (G::left_on_line && G::right_on_line){
+        while (G::left_on_line || G::right_on_line)
         {
             HW_API::read_data();   
             HW_API::backward();
