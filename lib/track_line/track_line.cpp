a8731c28bb1da6b3f0f164ea912713a1969d4edc
HenryCROSS <henry583050973@gmail.com>
2024-02-03 11:49:17 -0500

    update run()

--- lib/track_line/track_line.cpp
+++ lib/track_line/track_line.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
 
+#include <hardware_interface.hpp>
 #include <track_line.hpp>
 #include <configs.hpp>
 
 void Track_Line::run()
 {
+    HW_API::read_data();
+
+    if(G::left_on_line && G::right_on_line){
+
+    } else if (!G::left_on_line && G::right_on_line){
+
+    } else if (G::left_on_line && !G::right_on_line){
+        
+    } else if (!G::left_on_line && !G::right_on_line){
+        
+    } else {
+        
+    }
     
 }
