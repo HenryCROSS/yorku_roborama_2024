2fd7b3fd980d810bd2a1f50e3c19eca931b31126
HenryCROSS <henry583050973@gmail.com>
2024-02-03 17:13:46 -0500

    change speed

--- lib/configs/configs.cpp
+++ lib/configs/configs.cpp
@@ -8,11 +8,7 @@ int G::DELTA<Veh_Version::MK_1> = 10;
 template<>
 int G::SPEED<Veh_Version::MK_1> = 110;
 template<>
-<<<<<<< HEAD
-int G::TURN_SPEED<Veh_Version::MK_1> = 70;
-=======
 int G::TURN_SPEED<Veh_Version::MK_1> = 50;
->>>>>>> bd55b524aee5417dfd4ccd024261e41cf69d1c2f
 
 
 template<>
