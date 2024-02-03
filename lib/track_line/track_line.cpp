9e8b86a9b8b08736b02642e5afb006d1084b058e
Yongtai <yangyongtai924@gmail.com>
2024-02-03 11:59:10 -0500

    update run logic

--- lib/track_line/track_line.cpp
+++ lib/track_line/track_line.cpp
@@ -9,13 +9,17 @@ void Track_Line::run()
     HW_API::read_data();
 
     if(G::left_on_line && G::right_on_line){
-
+        HW_API::forward();
+        delay(10);
     } else if (!G::left_on_line && G::right_on_line){
-
+        HW_API::turn_right();
+        delay(10);
     } else if (G::left_on_line && !G::right_on_line){
-        
+        HW_API::turn_left();
+        delay(10);
     } else if (!G::left_on_line && !G::right_on_line){
-        
+        HW_API::backward();
+        delay(10);
     } else {
         
     }
