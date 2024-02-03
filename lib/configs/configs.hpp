f1009d773a05e4dce8f4485fe2c5565b95361cb2
Polynomial <ecnuzjy@qq.com>
2024-02-03 11:04:53 -0500

    change pin portal

--- lib/configs/configs.hpp
+++ lib/configs/configs.hpp
@@ -8,20 +8,16 @@ class G
 #define CS const static
 
 public:
-    CS int LEFT_MOTOR = 10;           // Motor A enable pin
-    CS int RIGHT_MOTOR = 11;          // Motor B enable pin
-    CS int RIGHT_WHEEL_FORWARD = 9;   // Motor B In1 pin
-    CS int RIGHT_WHEEL_BACKWARD = 12; // Motor B In2 pin
-    CS int LEFT_WHEEL_FORWARD = 7;    // Motor A In1 pin
-    CS int LEFT_WHEEL_BACKWARD = 8;   // Motor A In2 pin
-    CS int TRIGGER_PIN_L = 2;         // Ultrasonic sensor trigger pin
-    CS int ECHO_PIN_L = 3;            // Ultrasonic sensor echo pin
-    CS int TRIGGER_PIN_R = 4;         // Ultrasonic sensor trigger pin
-    CS int ECHO_PIN_R = 5;            // Ultrasonic sensor echo pin
-    CS int QTR_SENSOR_FL = A2;        // qtr sensor
-    CS int QTR_SENSOR_FR = A1;        // qtr sensor
-    CS int QTR_SENSOR_B = A0;         // qtr sensor
-    CS int DELTA = 0;
+    CS int LEFT_MOTOR = A0;           // Motor A enable pin
+    CS int RIGHT_MOTOR = A5;          // Motor B enable pin
+    CS int RIGHT_WHEEL_FORWARD = A4;   // Motor B In1 pin
+    CS int RIGHT_WHEEL_BACKWARD = A3; // Motor B In2 pin
+    CS int LEFT_WHEEL_FORWARD = A1;    // Motor A In1 pin
+    CS int LEFT_WHEEL_BACKWARD = A2;   // Motor A In2 pin
+    CS int SENSOR_L = A2;        // qtr sensor
+    CS int SENSOR_R = A1;        // qtr sensor
+
+    CS int DELTA = 0; // left speed will minus this value
 
     CS int SPEED = 100; // car speed
     
