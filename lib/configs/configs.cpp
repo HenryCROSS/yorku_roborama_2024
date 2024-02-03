bd55b524aee5417dfd4ccd024261e41cf69d1c2f
HenryCROSS <henry583050973@gmail.com>
2024-02-03 17:08:23 -0500

    change speed

--- lib/configs/configs.cpp
+++ lib/configs/configs.cpp
@@ -6,14 +6,14 @@ bool G::right_on_line = true;
 template<>
 int G::DELTA<Veh_Version::MK_1> = 0;
 template<>
-int G::SPEED<Veh_Version::MK_1> = 100;
+int G::SPEED<Veh_Version::MK_1> = 110;
 template<>
-int G::TURN_SPEED<Veh_Version::MK_1> = 75;
+int G::TURN_SPEED<Veh_Version::MK_1> = 50;
 
 
 template<>
 int G::DELTA<Veh_Version::MK_2> = 0;
 template<>
-int G::SPEED<Veh_Version::MK_2> = 100;
+int G::SPEED<Veh_Version::MK_2> = 110;
 template<>
-int G::TURN_SPEED<Veh_Version::MK_2> = 75;
+int G::TURN_SPEED<Veh_Version::MK_2> = 50;
