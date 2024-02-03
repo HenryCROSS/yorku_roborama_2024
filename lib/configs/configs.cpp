a124c454e01f50498db263e7be324e1e9af7f08d
Yongtai <yangyongtai924@gmail.com>
2024-02-03 17:13:56 -0500

    minor tunning

--- lib/configs/configs.cpp
+++ lib/configs/configs.cpp
@@ -6,13 +6,9 @@ bool G::right_on_line = true;
 template<>
 int G::DELTA<Veh_Version::MK_1> = 10;
 template<>
-int G::SPEED<Veh_Version::MK_1> = 110;
+int G::SPEED<Veh_Version::MK_1> = 120;
 template<>
-<<<<<<< HEAD
-int G::TURN_SPEED<Veh_Version::MK_1> = 70;
-=======
 int G::TURN_SPEED<Veh_Version::MK_1> = 50;
->>>>>>> bd55b524aee5417dfd4ccd024261e41cf69d1c2f
 
 
 template<>
